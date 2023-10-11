5952c65a2e0994bf0e36057b75a9eb16869da72c
Rajnish  Kumar <devrajnish@proton.me>
2023-10-11 14:21:50 +0530

    modified addfn

--- addfn.c
+++ addfn.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int addfn(int a, int b);
-int main(void)
+int addfn( int a , int b ) ;
+
+int main( void )
 {
   int a,b;
-  scanf("%d %d", &a, &b);
-  printf("Sum is %d", addfn(a, b));
+  scanf( "%d %d" , &a , &b  ) ;
+  printf("Sum is %d", addfn( a , b ) ) ;
 }
 
-int addfn(int a, int b)
+int addfn( int a , int b )
 {
   return (a+b);
 }
--- c-arg-add.c
+++ c-arg-add.c
@@ -5,8 +5,8 @@
 /// @retn
 int main(int argc, char *argv[]) {
 
-  long long int num1 = atoll(argv[1]);
-  long long int num2 = atoll(argv[2]);
+  long long  num1 = atoll(argv[1]);
+  long long  num2 = atoll(argv[2]);
 
   printf("  %lld + %lld = %lld \a", num1, num2, num1 + num2);
   exit(0);
