829c366fd7292b7a0ceff0b4de83f9b639bcf948
Rajnish  Kumar <devrajnish@proton.me>
2023-11-05 19:31:44 +0530

    clang-17.sh

---
+++ auto-run-sh/clang-17.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+clear
+full_filename="$1"
+
+filename_without_extension="${full_filename%.c}"
+
+c_directory="/home/rajnish/data/codes/c"
+
+c_file=$(find "$c_directory" -name "$full_filename" 2>/dev/null)
+
+lvim $full_filename && clang-17 -o "$c_directory/cx/$filename_without_extension" "$c_file" -Wall 
+
+ if [ $? -eq 0 ]; then
+
+                bat --style=full --theme "Monokai Extended" --paging never "$c_file"
+                printf "\e[1;33m |--   O U T P U T   --|" |  bat --style grid
+                $filename_without_extension ; printf "\n\n\n\n"
+                printf "                             \e[0;34m program exited with code : \e[1;32m $? " | bat --style=grid && read 
+
+fi
+  
+
+
